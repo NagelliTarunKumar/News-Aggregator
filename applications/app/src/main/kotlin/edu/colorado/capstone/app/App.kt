9f2a0caf7be541aae47b0ee6f0e45a0a9a2e5720
NagelliTarunKumar <135435439+NagelliTarunKumar@users.noreply.github.com>
2025-05-01 20:07:11 -0600

    Update App.kt

--- applications/app/src/main/kotlin/edu/colorado/capstone/app/App.kt
+++ applications/app/src/main/kotlin/edu/colorado/capstone/app/App.kt
@@ -10,12 +10,14 @@ import io.ktor.server.http.content.*
 import io.ktor.server.netty.*
 import io.ktor.server.routing.*
 import io.ktor.server.sessions.*
-import io.ktor.application.*
+
+import io.ktor.server.application.*
 import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.routing.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+
 fun Application.configured(
     databaseUrl: String,
 ) {
